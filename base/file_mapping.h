a282ff3b350fae857444e8003ad67fe51cf76f39
PhoXo <atphoxo@gmail.com>
2026-04-22 18:40:12 -0300

    minor update

--- base/file_mapping.h
+++ base/file_mapping.h
@@ -4,7 +4,7 @@ struct FCFileMapping
 {
     HANDLE    m_file{};
     HANDLE    m_mapping{};
-    LPCBYTE   m_data{};
+    LPCVOID   m_data{};
     LARGE_INTEGER   m_size{};
 
     FCFileMapping(PCWSTR path)
@@ -16,7 +16,7 @@ struct FCFileMapping
         m_mapping = CreateFileMapping(m_file, NULL, PAGE_READONLY, 0, 0, NULL);
         if (m_mapping)
         {
-            m_data = (LPCBYTE)MapViewOfFile(m_mapping, FILE_MAP_READ, 0, 0, 0);
+            m_data = MapViewOfFile(m_mapping, FILE_MAP_READ, 0, 0, 0);
             GetFileSizeEx(m_file, &m_size);
         }
     }
